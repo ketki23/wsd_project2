dbf2a86ca8c47ed41a4ebbb3f8a8fba2c3cff6f0
Ketki R Andhale <ketki.andhale@gmail.com>
2017-12-15 13:35:46 -0500

    all accounts

--- pages/all_accounts.php
+++ pages/all_accounts.php
@@ -2,23 +2,70 @@
 
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-
-    <title>The HTML5 Herald</title>
-    <meta name="description" content="The HTML5 Herald">
-    <meta name="author" content="SitePoint">
-
-    <link rel="stylesheet" href="css/styles.css?v=1.0">
-
-    <!--[if lt IE 9]>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
-    <![endif]-->
+  <title>Accounts</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+    
+    .navbar  /* Remove the navbar's default margin-bottom and rounded borders */ 
+    {
+      margin-bottom: 0;
+      border-radius: 0;
+    }
+    
+    .row.content {height: 515px} /* Set height of the grid  */
+     
+    .sidenav 
+    {
+      padding-top: 40px;
+      background-color: #f1f1f1;
+      height: 100%;
+    }
+    
+    
+    }
+       
+    }
+    </style>
 </head>
 
 <body>
-<h1>All Accounts</h1>
+    <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+  <div class="navbar-header">
+  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+  <span class="icon-bar"></span>
+  <span class="icon-bar"></span>
+  <span class="icon-bar"></span>                        
+  </button>
+  <a class="navbar-brand" href="#">Task Application</a>
+  </div>
+  <div class="collapse navbar-collapse" id="Navigationbar">
+  <ul class="nav navbar-nav">
+  <li class="active"><a href="index.php?page">Home</a></li>
+  <li><a href="index.php?page=accounts&action=all">All Accounts</a></li>
+  <li><a href="index.php?page=tasks&action=all">Todo Tasks</a></li>
+  <li><a href="index.php?page=tasks&action=create">Add task</a></li>
+  </ul>
+  <ul class="nav navbar-nav navbar-right">
+  </ul>
+  </div>
+  </div>
+  </nav>
+  
 
-<h1> HI</h1>
+  </div>
+  </form>
+  </div>
+  </div>
+  </div>
+  </div>
+
+<h2><div style="text-align:center">List of all Accounts</h2>
+
 
 <?php
 //this is how you print something
