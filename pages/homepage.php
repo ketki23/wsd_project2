c3a518380aa6bd0025988dcf6acfcb0cb911d3a7
Ketki R Andhale <ketki.andhale@gmail.com>
2017-12-15 02:27:05 -0500

    added

--- pages/homepage.php
+++ pages/homepage.php
@@ -3,6 +3,41 @@
 <html lang="en">
 <body style="background-color:tan;">
 <head>
+
+  <title>Task Application</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+    
+    .navbar  /* Remove the navbar's default margin-bottom and rounded borders */ 
+    {
+      margin-bottom: 0;
+      border-radius: 0;
+    }
+    
+    .row.content {height: 515px} /* Set height of the grid  */
+     
+    .sidenav 
+    {
+      padding-top: 40px;
+      background-color: #f1f1f1;
+      height: 100%;
+    }
+    
+    footer /* Set black background color */
+    {
+      background-color: #555;
+      color: white;
+      padding: 20px;
+    }
+    
+    }
+       
+    }
+    </style>
     
 </head>
 
