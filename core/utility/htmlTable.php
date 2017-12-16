5c68b31cd4098462235f307229ecd29e5fc2cd24
Ketki R Andhale <ketki.andhale@gmail.com>
2017-12-16 15:01:38 -0500

    updated

--- core/utility/htmlTable.php
+++ core/utility/htmlTable.php
@@ -8,7 +8,7 @@ class htmlTable
     public static function genarateTableFromMultiArray($array)
     {
 
-        $tableGen = '<table border="1"cellpadding="10">';
+        $tableGen = '<table border="3"cellpadding="15">';
         $tableGen .= '<tr>';
         //this grabs the first element of the array so we can extract the field headings for the table
         $fieldHeadings = $array[0];
@@ -16,16 +16,21 @@ class htmlTable
         $fieldHeadings = array_keys($fieldHeadings);
         //this gets the page being viewed so that the table routes requests to the correct controller
         $referingPage = $_REQUEST['page'];
-        foreach ($fieldHeadings as $heading) {
+        foreach ($fieldHeadings as $heading) 
+        {
             $tableGen .= '<th>' . $heading . '</th>';
         }
         $tableGen .= '</tr>';
-        foreach ($array as $record) {
+        foreach ($array as $record) 
+        {
             $tableGen .= '<tr>';
-            foreach ($record as $key => $value) {
-                if ($key == 'id') {
+            foreach ($record as $key => $value) 
+            {
+                if ($key == 'id') 
+                {
                     $tableGen .= '<td><a href="index.php?page=' . $referingPage . '&action=show&id=' . $value . '">View</a></td>';
-                } else {
+                } else 
+                {
                     $tableGen .= '<td>' . $value . '</td>';
                 }
             }
@@ -39,15 +44,17 @@ class htmlTable
 
     public static function generateTableFromOneRecord($innerArray)
     {
-        $tableGen = '<table border="1" cellpadding="10"><tr>';
+        $tableGen = '<table border="3" cellpadding="15"><tr>';
 
         $tableGen .= '<tr>';
-        foreach ($innerArray as $innerRow => $value) {
+        foreach ($innerArray as $innerRow => $value)
+        {
             $tableGen .= '<th>' . $innerRow . '</th>';
         }
         $tableGen .= '</tr>';
 
-        foreach ($innerArray as $value) {
+        foreach ($innerArray as $value) 
+        {
             $tableGen .= '<td>' . $value . '</td>';
         }
 
