46eeca967268f6158f51bd5887162e706fe9bd32
Ketki R Andhale <ketki.andhale@gmail.com>
2017-12-17 04:20:52 -0500

    updated

--- pages/show_task.php
+++ pages/show_task.php
@@ -2,25 +2,74 @@
 
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-
-    <title>The HTML5 Herald</title>
-    <meta name="description" content="The HTML5 Herald">
-    <meta name="author" content="SitePoint">
-
-    <link rel="stylesheet" href="css/styles.css?v=1.0">
-
-    <!--[if lt IE 9]>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
-    <![endif]-->
+     <title>Tasks</title>
+  <m eta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+    
+    .navbar  /* Remove the navbar's default margin-bottom and rounded borders */ 
+    {
+      margin-bottom: 0;
+      border-radius: 0;
+    }
+    
+    .row.content {height: 515px} /* Set height of the grid  */
+     
+    .sidenav 
+    {
+      padding-top: 40px;
+      background-color: #f1f1f1;
+      height: 100%;
+    }
+    
+    
+    }
+       
+    }
+    </style>
 </head>
 
 <body>
 
+ <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+  <div class="navbar-header">
+  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+  <span class="icon-bar"></span>
+  <span class="icon-bar"></span>
+  <span class="icon-bar"></span>                        
+  </button>
+  <a class="navbar-brand" href="#">Task Application</a>
+  </div>
+  <div class="collapse navbar-collapse" id="Navigationbar">
+  <ul class="nav navbar-nav">
+  <li class="active"><a href="index.php?page">Home</a></li>
+  <li><a href="index.php?page=accounts&action=all">All Accounts</a></li>
+  <li><a href="index.php?page=tasks&action=all">Todo Tasks</a></li>
+  <li><a href="index.php?page=tasks&action=create">Add task</a></li>
+  <li><a href="index.php?page">Log Out</a></li>
+  </ul>
+  <ul class="nav navbar-nav navbar-right">
+  </ul>
+  </div>
+  </div>
+  </nav>
+  
+
+  </div>
+  </form>
+  </div>
+  </div>
+  </div>
+  </div>
+  <div style="text-align:center"><br><br>
 <?php
 //this is how you print something  $data contains the record that was selected on the table.
-
-print_r($data);
+print utility\htmlTable::genarateTableFromMultiArray($data);
+//Echo 'You want to delete the task?<br><br>';
 ?>
 <form action="index.php?page=tasks&action=delete&id=<?php echo $data->id; ?> " method="post" id="form1">
     <button type="submit" form="form1" value="delete">Delete</button>
