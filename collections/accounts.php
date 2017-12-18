d381bf4261b3d6df3b91d437a7c866ad93f4e9bf
Ketki R Andhale <ketki.andhale@gmail.com>
2017-12-18 17:14:00 -0500

    refactored

--- collections/accounts.php
+++ collections/accounts.php
@@ -3,17 +3,17 @@
 class accounts extends \database\collection
 {
     protected static $modelName = 'account';
-
     public static function findUserbyEmail($email)
     {
-
             $tableName = get_called_class();
             $sql = 'SELECT * FROM ' . $tableName . ' WHERE email = ?';
             $recordsSet = self::getResults($sql, $email);
 
-            if (is_null($recordsSet)) {
+            if (is_null($recordsSet)) 
+            {
                 return FALSE;
-            } else {
+            } else 
+            {
                 return $recordsSet[0];
             }
     }
